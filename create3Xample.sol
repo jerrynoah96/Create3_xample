90be04b2725d855ff0911cb4439be857ccc02e85
USER <user@USERs-MacBook-Pro.local>
2022-10-13 04:16:03 -0700

    using create3 to deploy and functionality to verify that a contract was deployed by the deployerContract

---
+++ create3Xample.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "https://github.com/0xsequence/create3/blob/master/contracts/Create3.sol";
+
+
+contract ChildContract {
+  uint256 initNum;
+  address owner;
+  
+  constructor(uint256 _init) {
+    initNum = _init;
+    owner = msg.sender;
+  }
+}
+
+contract Deployer {
+
+  event ChildCreated(address _childContractAddress, string _ChildSalt);
+
+
+  /// @notice deploys childContract using create3
+  /// emits ChildCreated()
+  function deployChildContract(string memory _salt) external {
+   address _addr = Create3.create3(
+      keccak256(bytes(_salt)), 
+      abi.encodePacked(
+        type(ChildContract).creationCode,
+        abi.encode(
+          42
+        )
+      )
+    );
+
+    emit ChildCreated(_addr, _salt);
+  }
+
+
+  /// @notice confirms that an address was created by this contract providing a salt
+  function confirmChildContract(address _childContractToVerify, string memory salt_string) public view returns(bool){
+    bytes32 salt = keccak256(bytes(salt_string));
+    address _child = Create3.addressOf(salt);
+    require(_childContractToVerify == _child, "provided child not created by this contract");
+    return true;
+
+  }
+}
